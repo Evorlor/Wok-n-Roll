eec40b858518c8b44b5e18b699b06cdf42f821e7
Samuel Knox <sam@evorlor.com>
2016-01-16 11:46:34 -0500

    test input script

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,14 +1,43 @@
 ﻿using UnityEngine;
 
 public class Test : MonoBehaviour {
-	void Update () {
+	void Update ()
+    {
         if (Input.GetButton(InputNames.North))
         {
-            Debug.Log("north");
+            Debug.Log("North");
         }
-        if (Input.GetButton(InputNames.Northeast))
+        else if (Input.GetButton(InputNames.Northeast))
         {
-            Debug.Log("northeast");
+            Debug.Log("Northeast");
         }
-	}
+        else if (Input.GetButton(InputNames.East))
+        {
+            Debug.Log("East");
+        }
+        else if (Input.GetButton(InputNames.Southeast))
+        {
+            Debug.Log("Southeast");
+        }
+        else if (Input.GetButton(InputNames.South))
+        {
+            Debug.Log("South");
+        }
+        else if (Input.GetButton(InputNames.Southwest))
+        {
+            Debug.Log("Southwest");
+        }
+        else if (Input.GetButton(InputNames.West))
+        {
+            Debug.Log("West");
+        }
+        else if (Input.GetButton(InputNames.Northwest))
+        {
+            Debug.Log("Northwest");
+        }
+        else if (!Input.GetButton(InputNames.Jump))
+        {
+            Debug.Log("Jump");
+        }
+    }
 }
