0ff327be8a051752a9be9eba37ad6f0140637e2f
Samuel Knox <sam@evorlor.com>
2016-01-16 15:10:17 -0500

    animation controller

---
+++ Assets/Scripts/InstructionAnimator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class InstructionAnimator : MonoBehaviour
+{
+    private Animator animator;
+
+    private const string animationIntegerName = "Action";
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        animator.SetInteger(animationIntegerName, GetInstructionNumber());
+    }
+
+    /// <summary>
+    /// Updates the instruction's animation in the center of the screen
+    /// </summary>
+    public void UpdateAnimation()
+    {
+        animator.SetInteger(animationIntegerName, GetInstructionNumber());
+    }
+
+    private int GetInstructionNumber()
+    {
+        var currentInstruction = InstructionManager.Instance.GetCurrentInstruction();
+        if (currentInstruction)
+        {
+            return (int)currentInstruction.action;
+        }
+        return -1;
+    }
+}
