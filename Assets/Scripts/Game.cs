6e4091bdb939e277e4b008ad71a8bccb223b531c
Yuhsiang Lu <luki0639@gmail.com>
2016-01-17 13:51:47 -0500

    Update fix

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -62,9 +62,9 @@ public class Game : MonoBehaviour {
 
         timerText.text = timeStr;
 
-        bool isJumping = Control.GetInstance().GetInput(Action.North);
+        bool isJumping = Control.GetInstance().GetInput(Action.Jump);
 
-      if (gameOver.IsGameOver() && isJumping){
+        if (gameOver.IsGameOver() && isJumping){
             gameOver.SetGameOver(false);
             gameOver.image.enabled = false;
             StartTime = defaultTime;
