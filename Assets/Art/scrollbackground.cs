c0ae6a8a6f5bdd0bb171b645827ffaaaa104aa56
unknown <ajordan@ajordan.fiea.local>
2016-01-17 02:10:54 -0500

    all the shit

---
+++ Assets/Art/scrollbackground.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollbackground : MonoBehaviour {
+
+    private Renderer rendererfriend;
+
+    public float speed = 0f;
+    public float xOffset = 0f;
+    public GameObject map;
+    
+    void Awake()
+    {
+        rendererfriend = GetComponent<Renderer>();
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rendererfriend.material.mainTextureOffset = new Vector2(Time.time * xOffset, Time.time * speed);
+        
+
+    }
+}
